4a9498325ae9c080926228ea9059e846dc984f22
Bartosz Lewandowski <lewandowskibartosz@icloud.com>
2018-10-21 12:46:27 +0200

    extend test for change project usecase

--- tests/Core/Domain/ProjectTest.php
+++ tests/Core/Domain/ProjectTest.php
@@ -13,7 +13,7 @@ class ProjectTests extends TestCase
 {
     public function testShouldCreateProject()
     {
-        $project = new Project('Web Application', 'WEBAPP', new \DateTimeImmutable());
+        $project = self::createValidProject();
 
         Assert::assertNotNull($project->getId());
         Assert::assertEquals('Web Application', $project->getName());
@@ -34,5 +34,29 @@ class ProjectTests extends TestCase
     public function invalidProjectNamesDataProvider(): array
     {
         return [['', 'Inva', 'It is not the best name for project cause it is to long']];
+    }
+
+    public function testShouldChangeProjectName()
+    {
+        $project = self::createValidProject();
+        $project->changeName('Web Managment Application');
+
+        Assert::assertInstanceOf(\DateTimeInterface::class, $project->getUpdatedAt());
+        Assert::assertEquals('Web Managment Application', $project->getName());
+    }
+
+    /**
+     * @dataProvider invalidProjectNamesDataProvider
+     */
+    public function testShouldThrowExceptionWhileChangingProjectName($name)
+    {
+        $this->expectException(InvalidProjectNameException::class);
+        $project = self::createValidProject();
+        $project->changeName($name);
+    }
+
+    private static function createValidProject(): Project
+    {
+        return new Project('Web Application', 'WEBAPP', new \DateTimeImmutable());
     }
 }
