b683ef7d0e1b785f69ac5d8ce64e20f9c16748a3
Bartosz Lewandowski <lewandowskibartosz@icloud.com>
2018-10-23 22:26:54 +0200

    add tests for checking duplicates in projects

--- tests/Core/Application/ProjectServiceTest.php
+++ tests/Core/Application/ProjectServiceTest.php
@@ -6,12 +6,15 @@ namespace App\Tests\Core\Application;
 use App\Core\Application\ProjectService;
 use App\Core\Application\Command\CreateProjectCommand;
 use App\Core\Application\DTO\ProjectDTO;
+use App\Core\Domain\Project;
+use App\Core\Domain\Event\ProjectWasCreated;
+use App\Core\Domain\Exception\DuplicateDetectedException;
+use App\Core\Domain\Repository\ProjectReadRepository;
 use App\Core\Domain\Repository\ProjectWriteRepository;
 use PHPUnit\Framework\Assert;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\MockObject\MockObject;
-use App\Core\Domain\Project;
-use App\Core\Domain\Event\ProjectWasCreated;
+use Ramsey\Uuid\Uuid;
 
 class ProjectServiceTest extends TestCase
 {
@@ -21,16 +24,32 @@ class ProjectServiceTest extends TestCase
     /** @var ProjectWriteRepository|MockObject */
     private $projectWriteRepositoryMock;
 
+    /** @var ProjectReadRepository|MockObject */
+    private $projectReadRepositoryMock;
+
     public function setUp()
     {
         $this->projectWriteRepositoryMock = $this->createMock(ProjectWriteRepository::class);
-        $this->systemUnderTest = new ProjectService($this->projectWriteRepositoryMock);
+        $this->projectReadRepositoryMock = $this->createMock(ProjectReadRepository::class);
+        $this->systemUnderTest = new ProjectService(
+            $this->projectWriteRepositoryMock,
+            $this->projectReadRepositoryMock
+        );
     }
 
-    public function testShouldCreateProjectFromCommand()
+    public function testShouldCreateProjectFromDTO()
     {
+        $this->projectReadRepositoryMock->method('exists')->willReturn(false);
         $projectWasCrated = $this->systemUnderTest->create(new ProjectDTO('Project Name', 'SHORT'));
 
         Assert::assertInstanceOf(ProjectWasCreated::class, $projectWasCrated);
+    }
+
+    public function testShouldThrowDuplicateDetected()
+    {
+        $this->expectException(DuplicateDetectedExcepton::class);
+
+        $this->projectReadRepositoryMock->method('exists')->willReturn(true);
+        $this->systemUnderTest->create(new ProjectDTO('Project Name', 'SHORT'));
     }
 }
